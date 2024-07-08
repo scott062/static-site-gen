36bb9a51f8b369dd2fff5338675f7d3cbe9024a8
scott062 <scott.m.burdick@gmail.com>
2024-07-08 18:26:34 -0400

    Add htmlnode

---
+++ src/htmlnode.py
@@ -0,0 +1,22 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+    def to_html(self):
+        raise NotImplementedError()
+
+    def props_to_html(self):
+        if self.props:
+            return "".join([f" {k}={v}" for k,v in self.props.items()])
+        return ""
+             
+    
+            
+
+        
---
+++ src/test_htmlnode.py
@@ -0,0 +1,22 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_empty_props_to_html(self):
+        node = HTMLNode()
+        self.assertEqual(node.props_to_html(), "")
+
+    def test_props_to_html(self):
+        node = HTMLNode(None, None, None, { "htmlProp": "first", "htmlPropAgain": "second" })
+        self.assertEqual(node.props_to_html(), " htmlProp=first htmlPropAgain=second")
+
+    def test_repr(self):
+        node = HTMLNode("p", "gigi", None, { "htmlProp": "first", "htmlPropAgain": "second" })
+        self.assertEqual(repr(node), "HTMLNode(p, gigi, None, {'htmlProp': 'first', 'htmlPropAgain': 'second'})")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
