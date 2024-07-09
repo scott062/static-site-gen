72b7fda532161dad918c4f784af75d50566ba5a8
scott062 <scott.m.burdick@gmail.com>
2024-07-08 22:44:10 -0400

    Add text to html node

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,7 @@
+from enum import Enum
+from leafnode import LeafNode
+
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -15,5 +19,25 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})" 
-        
-        
+
+
+Texttype = Enum("Texttype", ["text", "bold", "italic", "code", "link", "image"])
+
+def text_node_to_html_node(text_node):
+    content = text_node.value
+    match (text_node.text_type):
+        case (Texttype.text):
+            return LeafNode(None, content)
+        case (Texttype.bold):
+            return LeafNode("b", content)
+        case (Texttype.italic):
+            return LeafNode("i", content)
+        case (Texttype.code):
+            return LeafNode("code", content)
+        case (Texttype.link):
+            return LeafNode("a", content, {"href": text_node.url})
+        case (Texttype.image):
+            return LeafNode("img", "", {"src": text_node.url, "alt": content})
+        case _:
+            raise Exception("Not a valid text type")
+
