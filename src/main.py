8edbcc9114aff89d34fea3c8fd506c7a0f5b6ad8
scott062 <scott.m.burdick@gmail.com>
2024-09-01 18:23:38 -0400

    Add static generator to main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,36 @@
 from textnode import TextNode
+import os
+import shutil
 
 def main():
-    a = TextNode("Scott", "Brittany", "NoriGigi")
-    b = TextNode("Scott", "Brittany", "oriGigi")
-    print(a.__repr__())
-    print(b.__repr__())
-    print(b.__eq__(a))
-    print(a.__eq__(b))
+    cleanup()
+    generate()
+
+def cleanup():
+    if os.path.exists("./public"):
+            shutil.rmtree("./public")
+    os.mkdir("./public")
+
+def generate(src="./static", dst="./public"):
+    contents = os.listdir(src)
+
+    if not contents:
+        return
+
+    if not os.path.exists(dst):
+        os.mkdir(dst)
+
+    for item in contents:
+        item_src = os.path.join(src, item)
+        item_dst = os.path.join(dst, item)
+        if os.path.isfile(item_src):
+            shutil.copy(item_src, item_dst)
+        else:
+            generate(item_src, item_dst)
+
+
+
+
+
 
 main()
